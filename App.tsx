5130e7f5f2a76f355071f1f2fe6a96f03f234cf1
renanribeir <renanribeirow19@gmail.com>
2022-01-12 15:04:12 -0300

    inclui uso do svg

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
+import IconBook from './assets/book.svg';
+
 const App = () => {
   return (
-    <View>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Svg Test</Text>
+      <IconBook fill="#000" width={120} height={120} />
     </View>
   );
 };
